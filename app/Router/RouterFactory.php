07cc824e920427f388224470bf16ff07af533e91
pilarik31 <pilarjan4111@gmail.com>
2021-12-01 15:02:59 +0100

    Fix module routing

--- app/Router/RouterFactory.php
+++ app/Router/RouterFactory.php
@@ -15,7 +15,7 @@ final class RouterFactory
 	public static function createRouter(): RouteList
 	{
 		$router = new RouteList;
-		$router->addRoute('<presenter>/<action>', 'Admin:Dashboard:default');
+		$router->addRoute('<module>/<presenter>/<action>', 'Admin:Dashboard:default');
 		return $router;
 	}
 }
