68f9478012aa65f058a9aaa1f6a673ca01d7fe55
David <david@grudl.com>
2023-10-09 05:09:36 +0200

    Error4xxPresenter: handles 405 error

--- app/Module/Auxiliary/Presenters/Error4xxPresenter.php
+++ app/Module/Auxiliary/Presenters/Error4xxPresenter.php
@@ -8,28 +8,27 @@ use Nette;
 
 
 /**
- * Handles 4xx HTTP error responses, such as 403 Forbidden or 404 Not Found.
+ * Handles 4xx HTTP error responses.
  */
 final class Error4xxPresenter extends Nette\Application\UI\Presenter
 {
-	/**
-	 * Ensures the request is a forward (internal redirect).
-	 */
-	public function startup(): void
+	protected function checkHttpMethod(): void
 	{
-		parent::startup();
+		// allow access via all HTTP methods and ensure the request is a forward (internal redirect)
 		if (!$this->getRequest()->isMethod(Nette\Application\Request::FORWARD)) {
 			$this->error();
 		}
 	}
 
 
-	/**
-	 * Renders the appropriate error template based on the HTTP status code.
-	 */
 	public function renderDefault(Nette\Application\BadRequestException $exception): void
 	{
-		$file = __DIR__ . "/templates/Error/{$exception->getCode()}.latte";
-		$this->template->setFile(is_file($file) ? $file : __DIR__ . '/templates/Error/4xx.latte');
+		// renders the appropriate error template based on the HTTP status code
+		$code = $exception->getCode();
+		$file = is_file($file = __DIR__ . "/templates/Error/$code.latte")
+			? $file
+			: __DIR__ . '/templates/Error/4xx.latte';
+		$this->template->httpCode = $code;
+		$this->template->setFile($file);
 	}
 }
